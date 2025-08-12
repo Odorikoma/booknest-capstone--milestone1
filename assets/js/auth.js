e5d84f1ee1fa433afc6bb42cbda86d77ee2edfae
Odorikoma <mahaokun89@gmail.com>
2025-08-12 18:12:20 -0500

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -27,7 +27,7 @@ window.onload = () => {
     const password = document.getElementById('login-password').value;
 
     try {
-      const response = await fetch('http://127.0.0.1:8080/api/auth/login', {
+      const response = await fetch('${API_BASE_URL}/api/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
@@ -63,7 +63,7 @@ window.onload = () => {
     const password = document.getElementById('register-password').value;
 
     try {
-      const response = await fetch('http://127.0.0.1:8080/api/auth/register', {
+      const response = await fetch('${API_BASE_URL}/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, email, password }),
