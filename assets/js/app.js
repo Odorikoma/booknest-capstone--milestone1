51ffbda4b219df131cfab8503399f0696baa8937
RazRadish <68384311+RazRadish@users.noreply.github.com>
2025-08-14 16:14:36 -0500

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -38,7 +38,9 @@ window.onload = () => {
           <span class="text-sm font-semibold ${Number(book.stock) > 0 ? 'text-green-600' : 'text-red-600'}">
             ${Number(book.stock) > 0 ? `${book.stock} in stock` : 'Out of stock'}
           </span>
-          <a href="/book-detail.html?id=${encodeURIComponent(book.id)}" class="text-primary hover:underline">
+          <a href="/book-detail.html?id=${encodeURIComponent(book.id)}"
+             data-id="${book.id}"
+             class="text-primary hover:underline book-detail-link">
             View Details
           </a>
         </div>
